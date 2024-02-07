1b11ee1b800c8f5985ea86a070f16fa60ae4fda2
Matt Dainty <matt@bodgit-n-scarper.com>
2024-02-07 00:21:57 +0000

    refactor: Use built in `min()` & `max()` on 1.21+

--- internal/bra/bra.go
+++ internal/bra/bra.go
@@ -4,19 +4,3 @@ type converter interface {
 	Size() int
 	Convert([]byte, bool) int
 }
-
-func max(x, y int) int {
-	if x > y {
-		return x
-	}
-
-	return y
-}
-
-func min(x, y int) int {
-	if x < y {
-		return x
-	}
-
-	return y
-}
