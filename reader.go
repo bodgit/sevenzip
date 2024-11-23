fc4b13bde3b6960b66b844796006ac2d34bd5b25
Matt Dainty <matt@bodgit-n-scarper.com>
2024-11-23 00:40:45 +0000

    refactor: Change `io/fs` import

--- reader.go
+++ reader.go
@@ -9,7 +9,7 @@ import (
 	"fmt"
 	"hash/crc32"
 	"io"
-	"io/fs"
+	iofs "io/fs"
 	"os"
 	"path"
 	"path/filepath"
@@ -83,7 +83,7 @@ type fileReader struct {
 	n  int64
 }
 
-func (fr *fileReader) Stat() (fs.FileInfo, error) {
+func (fr *fileReader) Stat() (iofs.FileInfo, error) {
 	return headerFileInfo{&fr.f.FileHeader}, nil
 }
 
@@ -217,7 +217,7 @@ func OpenReaderWithPassword(name, password string) (*ReadCloser, error) {
 		for i := 2; true; i++ {
 			f, err := os.Open(fmt.Sprintf("%s.%03d", strings.TrimSuffix(name, ext), i))
 			if err != nil {
-				if errors.Is(err, fs.ErrNotExist) {
+				if errors.Is(err, iofs.ErrNotExist) {
 					break
 				}
 
@@ -582,8 +582,8 @@ type fileListEntry struct {
 }
 
 type fileInfoDirEntry interface {
-	fs.FileInfo
-	fs.DirEntry
+	iofs.FileInfo
+	iofs.DirEntry
 }
 
 func (e *fileListEntry) stat() (fileInfoDirEntry, error) {
@@ -604,11 +604,11 @@ func (e *fileListEntry) Name() string {
 	return elem
 }
 
-func (e *fileListEntry) Size() int64       { return 0 }
-func (e *fileListEntry) Mode() fs.FileMode { return fs.ModeDir | 0o555 }
-func (e *fileListEntry) Type() fs.FileMode { return fs.ModeDir }
-func (e *fileListEntry) IsDir() bool       { return true }
-func (e *fileListEntry) Sys() interface{}  { return nil }
+func (e *fileListEntry) Size() int64         { return 0 }
+func (e *fileListEntry) Mode() iofs.FileMode { return iofs.ModeDir | 0o555 }
+func (e *fileListEntry) Type() iofs.FileMode { return iofs.ModeDir }
+func (e *fileListEntry) IsDir() bool         { return true }
+func (e *fileListEntry) Sys() interface{}    { return nil }
 
 func (e *fileListEntry) ModTime() time.Time {
 	if e.file == nil {
@@ -618,7 +618,7 @@ func (e *fileListEntry) ModTime() time.Time {
 	return e.file.FileHeader.Modified.UTC()
 }
 
-func (e *fileListEntry) Info() (fs.FileInfo, error) { return e, nil }
+func (e *fileListEntry) Info() (iofs.FileInfo, error) { return e, nil }
 
 func toValidName(name string) string {
 	name = strings.ReplaceAll(name, `\`, `/`)
@@ -708,16 +708,16 @@ func fileEntryLess(x, y string) bool {
 // Open opens the named file in the 7-zip archive, using the semantics of
 // [fs.FS.Open]: paths are always slash separated, with no leading / or ../
 // elements.
-func (z *Reader) Open(name string) (fs.File, error) {
+func (z *Reader) Open(name string) (iofs.File, error) {
 	z.initFileList()
 
-	if !fs.ValidPath(name) {
-		return nil, &fs.PathError{Op: "open", Path: name, Err: fs.ErrInvalid}
+	if !iofs.ValidPath(name) {
+		return nil, &iofs.PathError{Op: "open", Path: name, Err: iofs.ErrInvalid}
 	}
 
 	e := z.openLookup(name)
 	if e == nil {
-		return nil, &fs.PathError{Op: "open", Path: name, Err: fs.ErrNotExist}
+		return nil, &iofs.PathError{Op: "open", Path: name, Err: iofs.ErrNotExist}
 	}
 
 	if e.isDir {
@@ -729,7 +729,7 @@ func (z *Reader) Open(name string) (fs.File, error) {
 		return nil, err
 	}
 
-	return rc.(fs.File), nil //nolint:forcetypeassert
+	return rc.(iofs.File), nil //nolint:forcetypeassert
 }
 
 func split(name string) (dir, elem string) {
@@ -800,16 +800,16 @@ type openDir struct {
 	offset int
 }
 
-func (d *openDir) Close() error               { return nil }
-func (d *openDir) Stat() (fs.FileInfo, error) { return d.e.stat() }
+func (d *openDir) Close() error                 { return nil }
+func (d *openDir) Stat() (iofs.FileInfo, error) { return d.e.stat() }
 
 var errIsDirectory = errors.New("is a directory")
 
 func (d *openDir) Read([]byte) (int, error) {
-	return 0, &fs.PathError{Op: "read", Path: d.e.name, Err: errIsDirectory}
+	return 0, &iofs.PathError{Op: "read", Path: d.e.name, Err: errIsDirectory}
 }
 
-func (d *openDir) ReadDir(count int) ([]fs.DirEntry, error) {
+func (d *openDir) ReadDir(count int) ([]iofs.DirEntry, error) {
 	n := len(d.files) - d.offset
 	if count > 0 && n > count {
 		n = count
@@ -823,7 +823,7 @@ func (d *openDir) ReadDir(count int) ([]fs.DirEntry, error) {
 		return nil, io.EOF
 	}
 
-	list := make([]fs.DirEntry, n)
+	list := make([]iofs.DirEntry, n)
 	for i := range list {
 		s, err := d.files[d.offset+i].stat()
 		if err != nil {
